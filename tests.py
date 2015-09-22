986909c55446a4b0181b87f4e764a25b603b0d2f
rochars <rocha.rafaelsilva@gmail.com>
2015-09-22 00:20:59 -0300

    tests.py PEP8

--- tests.py
+++ tests.py
@@ -5,7 +5,11 @@ import unittest
 
 from tests import *
 
-cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+cmd_folder = os.path.realpath(
+    os.path.abspath(
+        os.path.split(inspect.getfile(inspect.currentframe() ))[0]
+    )
+)
 if cmd_folder not in sys.path:
     sys.path.insert(0, cmd_folder)
 
