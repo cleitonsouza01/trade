15a4bcbc28a89bf25c445dd58bc240b38b9501d5
rochars <rocha.rafaelsilva@gmail.com>
2015-09-22 20:14:30 -0300

    AssetAccumulator.accumulate_trade()

--- trade_tools/trade_tools.py
+++ trade_tools/trade_tools.py
@@ -275,35 +275,35 @@ class Daytrade:
 
 
 class AssetAccumulator:
-    """ A accumulator of quantity @ some average price.
+	""" A accumulator of quantity @ some average price.
 
-    Attributes:
-        asset = An asset instance.
-        date = A string 'YYYY-mm-dd' representing the date of the last
-            status change of the accumulator
-        quantity = The asset's accumulated quantity.
-        price = The asset's average unitary price for the quantity
+	Attributes:
+	    asset = An asset instance.
+	    date = A string 'YYYY-mm-dd' representing the date of the last
+	        status change of the accumulator
+	    quantity = The asset's accumulated quantity.
+	    price = The asset's average unitary price for the quantity
 			accumulated.
-        results = A dict with the total results from from the trades
+	    results = A dict with the total results from from the trades
 			accumulated by the accumulator.
-        log_operations = A boolean indicating if the accumulator should
-            log the calls to the accumulate() method.
-        log = A dict with all the operations performed with the asset,
-            provided that self.log_operations is True.
+	    log_operations = A boolean indicating if the accumulator should
+	        log the calls to the accumulate() method.
+	    log = A dict with all the operations performed with the asset,
+	        provided that self.log_operations is True.
 
-    if created with log_operations=True the accumulator will keep a
-    log of the data of every trade it accumulate.
+	if created with log_operations=True the accumulator will keep a
+	log of the data of every trade it accumulate.
 
-    Results are calculated by the accumulator according to the value
-    of the trades informed and the accumulator current status of the
+	Results are calculated by the accumulator according to the value
+	of the trades informed and the accumulator current status of the
 	accumulator (the current quantity and average price of the asset).
 
-    The method accumulate() can take a optional param 'results', a dict
-    with other results to be included on the accumulator results dict
+	The method accumulate() can take a optional param 'results', a dict
+	with other results to be included on the accumulator results dict
 	and on the trade log.
-    """
+	"""
 
-    def __init__(self, asset, initial_status=None, log_operations=False):
+	def __init__(self, asset, initial_status=None, log_operations=False):
 		"""Create a instance of the accumulator.
 
 		A initial status (quantity, average price and results) can be
@@ -345,153 +345,162 @@ class AssetAccumulator:
 		self.log_operations = log_operations
 		self.log = {}
 
-    def log_operation(self, quantity, price, date, results):
-        """Log a operation.
+	def log_operation(self, quantity, price, date, results):
+	    """Log a operation.
 
-        If self.log_operations is True, then this method is callled
-        behind the scenes every time the method accumulate() is called.
+	    If self.log_operations is True, then this method is callled
+	    behind the scenes every time the method accumulate() is called.
 
-        The operations are logged like this:
+	    The operations are logged like this:
 
-            self.log = {
-                '2017-09-19': {
-                    'position': {
-                        'quantity': float
-                        'price': float
-                    }
-                    'operations': [
-                        {
-                            'quantity': float,
-                            'price': float,
-                            'results': {
-                                'result name': float
-                            }
-                        },
-                        ...
-                    ],
-                },
-                ...
-            }
-        """
+	        self.log = {
+	            '2017-09-19': {
+	                'position': {
+	                    'quantity': float
+	                    'price': float
+	                }
+	                'operations': [
+	                    {
+	                        'quantity': float,
+	                        'price': float,
+	                        'results': {
+	                            'result name': float
+	                        }
+	                    },
+	                    ...
+	                ],
+	            },
+	            ...
+	        }
+	    """
 
 		#If the date is not present in the dict keys,
 		# a new key created.
-        if date not in self.log:
-            self.log[date] = {'operations': []}
+	    if date not in self.log:
+	        self.log[date] = {'operations': []}
 
 		# Log the accumulator status after processing
 		# the trade
-        self.log[date]['position'] = {
-            'quantity': self.quantity,
-            'price': self.price,
-        }
+	    self.log[date]['position'] = {
+	        'quantity': self.quantity,
+	        'price': self.price,
+	    }
 
 		# Log the trade data
-        self.log[date]['operations'].append(
-            {
-                'quantity': quantity,
-                'price': price,
-                'results': results,
-            }
-        )
+	    self.log[date]['operations'].append(
+	        {
+	            'quantity': quantity,
+	            'price': price,
+	            'results': results,
+	        }
+	    )
 
-    def accumulate(self, quantity, price, date=None, results=None):
-        """Accumulate a trade to the existing position.
+	def accumulate(self, quantity, price, date=None, results=None):
+	    """Accumulate trade data to the existing position.
 
-        The method accumulate() can take a optional param 'results',
-        a dict with other results to be included on the accumulator
-        results dict.
+	    The method accumulate() can take a optional param 'results',
+	    a dict with other results to be included on the accumulator
+	    results dict.
 
-        For example, if in 2015-09-19 you had 100 of *something* at the
-        price of $10.45, and then proceeded to buy 10 more for 10
-        dollars, then sold 10 on the same day for 20 dollars, you had a
-        result related to this stock, but not a change in position. In
-        this case you would call the accumulate() method like this:
+	    For example, if in 2015-09-19 you had 100 of *something* at the
+	    price of $10.45, and then proceeded to buy 10 more for 10
+	    dollars, then sold 10 on the same day for 20 dollars, you had a
+	    result related to this stock, but not a change in position. In
+	    this case you would call the accumulate() method like this:
 
-            accumulator.accumulate(
-                date='2015-09-19',
-                results={
-                    'daytrade': 100
-                }
-            )
+	        accumulator.accumulate(
+	            date='2015-09-19',
+	            results={
+	                'daytrade': 100
+	            }
+	        )
 
 		The result dict passed to this method must obey the format:
 
-            results = {
-                'result name': float
-            }
+	        results = {
+	            'result name': float
+	        }
 
 		The accumulator take care of adding the custom results to the
 		total results of the stock on its own results attribute, wich
 		is a dict like this:
 
-            self.results = {
-                'result name': float,
-                ...
-            }
+	        self.results = {
+	            'result name': float,
+	            ...
+	        }
 
 		The custom results will also be logged, if logging.
-        """
+	    """
 
-        new_quantity = self.quantity + quantity
+	    new_quantity = self.quantity + quantity
 
-        if results is None:
-            results = {'trade': 0}
+	    if results is None:
+	        results = {'trade': 0}
 
-        # if the quantity of the trade has the same sign
+	    # if the quantity of the trade has the same sign
 		# of the accumulated quantity then we need to
 		# find out the new average price of the asset
-        if same_sign(self.quantity, quantity):
+	    if same_sign(self.quantity, quantity):
 
-            # if the new quantity is zero, then the new average
-            # price is also zero; otherwise, we need to calc the
-            # new average price
-            if new_quantity:
-                new_price = average_price(
-                                self.quantity,
-                                self.price,
-                                quantity,
-                                price
-                            )
-            else:
-                new_price = 0
+	        # if the new quantity is zero, then the new average
+	        # price is also zero; otherwise, we need to calc the
+	        # new average price
+	        if new_quantity:
+	            new_price = average_price(
+	                            self.quantity,
+	                            self.price,
+	                            quantity,
+	                            price
+	                        )
+	        else:
+	            new_price = 0
 
 		# If the traded quantity has an opposite sign of the
 		# asset's accumulated quantity, then there was a
 		# result.
-        else:
+	    else:
 
-            # If the new accumulated quantity is of the same sign
-            # of the old accumulated quantity, the average of price
-            # will not change.
-            if same_sign(self.quantity, new_quantity):
-                new_price = self.price
+	        # If the new accumulated quantity is of the same sign
+	        # of the old accumulated quantity, the average of price
+	        # will not change.
+	        if same_sign(self.quantity, new_quantity):
+	            new_price = self.price
 
-            # If the new accumulated quantity is of different
-            # sign of the old accumulated quantity then the
-            # average price is now the price of the operation
-            else:
-                new_price = price
+	        # If the new accumulated quantity is of different
+	        # sign of the old accumulated quantity then the
+	        # average price is now the price of the operation
+	        else:
+	            new_price = price
 
-            # calculate the result of this operation and add
-            # the new result to the accumulated results
-            results['trade'] += abs(quantity)*price - abs(quantity)*self.price
+	        # calculate the result of this operation and add
+	        # the new result to the accumulated results
+	        results['trade'] += abs(quantity)*price - abs(quantity)*self.price
 
-        # update the accumulator quantity and average
-        # price with the new values
-        self.quantity = new_quantity
-        if new_quantity:
-            self.price = new_price
-        else:
-            self.price = 0
+	    # update the accumulator quantity and average
+	    # price with the new values
+	    self.quantity = new_quantity
+	    if new_quantity:
+	        self.price = new_price
+	    else:
+	        self.price = 0
 
-        # add whatever result was informed with or generated
-        # by this operation to the accumulator results dict
-        for key, value in results.iteritems():
-            if key not in self.results:
-                self.results[key] = 0
-            self.results[key] += value
+	    # add whatever result was informed with or generated
+	    # by this operation to the accumulator results dict
+	    for key, value in results.iteritems():
+	        if key not in self.results:
+	            self.results[key] = 0
+	        self.results[key] += value
 
-        # log the operation, if logging
-        if self.log_operations:
-            self.log_operation(quantity, price, date, results)
+	    # log the operation, if logging
+	    if self.log_operations:
+	        self.log_operation(quantity, price, date, results)
+
+	def accumulate_trade(self, trade):
+		"""Accumulate a trade to the existing position.
+		"""
+		self.accumulate(
+			trade.quantity,
+			trade.price,
+			date=trade.date
+		)
