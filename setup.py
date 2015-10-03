32b082d4d79a1db278ddbd204a8290dfee4edb0c
rochars <rocha.rafaelsilva@gmail.com>
2015-10-03 18:44:54 -0300

    fix reference to original sample in setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
