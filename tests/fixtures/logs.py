fe6d9a5dbf199dbe6ecf14833ecc39134f6770e4
rochars <rocha.rafaelsilva@gmail.com>
2015-11-07 23:28:07 -0200

    log fixtures using accumulator states

--- tests/fixtures/logs.py
+++ tests/fixtures/logs.py
@@ -1,159 +1,59 @@
 """A set of logs for the accumulator tests."""
 
+from .accumulator_states import (
+    EXPECTED_STATE1, EXPECTED_STATE9, EXPECTED_STATE7, EXPECTED_STATE10,
+    EXPECTED_STATE11, EXPECTED_STATE25, EXPECTED_STATE2, EXPECTED_STATE23,
+    EXPECTED_STATE24, EXPECTED_STATE12, EXPECTED_STATE0, EXPECTED_STATE3,
+    EXPECTED_STATE13, EXPECTED_STATE4, EXPECTED_STATE5, EXPECTED_STATE6,
+    EXPECTED_STATE8
+)
+
 EXPECTED_LOG0 = {
-    '2015-01-02': {
-        'quantity': 0,
-        'price': 0,
-        'results': {},
-    },
-    '2015-01-01': {
-        'quantity': 100,
-        'price': 10,
-        'results': {},
-    }
+    '2015-01-02': EXPECTED_STATE1,
+    '2015-01-01': EXPECTED_STATE9
 }
 
 EXPECTED_LOG1 = {
-    '2015-01-03': {
-        'quantity': 100,
-        'price': 10,
-        'results': {},
-    },
-    '2015-01-02': {
-        'quantity': 0,
-        'price': 0,
-        'results': {},
-    },
-    '2015-01-01': {
-        'quantity': 100,
-        'price': 10,
-        'results': {},
-    }
+    '2015-01-03': EXPECTED_STATE9,
+    '2015-01-02': EXPECTED_STATE1,
+    '2015-01-01': EXPECTED_STATE9
 }
 
 EXPECTED_LOG2 = {
-    '2015-01-04': {
-        'quantity': 0,
-        'price': 0,
-        'results': {'trades': 1000},
-    },
-    '2015-01-03': {
-        'quantity': 100,
-        'price': 10,
-        'results': {},
-    },
-    '2015-01-02': {
-        'quantity': 0,
-        'price': 0,
-        'results': {},
-    },
-    '2015-01-01': {
-        'quantity': 100,
-        'price': 10,
-        'results': {},
-    }
+    '2015-01-04': EXPECTED_STATE7,
+    '2015-01-03': EXPECTED_STATE9,
+    '2015-01-02': EXPECTED_STATE1,
+    '2015-01-01': EXPECTED_STATE9
 }
 
 EXPECTED_LOG3 = {
-    '2015-01-05': {
-        'quantity': 100,
-        'price': 20,
-        'results': {'trades': 1000},
-    },
-    '2015-01-04': {
-        'quantity': 0,
-        'price': 0,
-        'results': {'trades': 1000},
-    },
-    '2015-01-03': {
-        'quantity': 100,
-        'price': 10,
-        'results': {},
-    },
-    '2015-01-02': {
-        'quantity': 0,
-        'price': 0,
-        'results': {},
-    },
-    '2015-01-01': {
-        'quantity': 100,
-        'price': 10,
-        'results': {},
-    }
+    '2015-01-05': EXPECTED_STATE10,
+    '2015-01-04': EXPECTED_STATE7,
+    '2015-01-03': EXPECTED_STATE9,
+    '2015-01-02': EXPECTED_STATE1,
+    '2015-01-01': EXPECTED_STATE9
 }
 
 EXPECTED_LOG4 = {
-    '2015-01-06': {
-        'quantity': 0,
-        'price': 0,
-        'results': {'trades': 3000},
-    },
-    '2015-01-05': {
-        'quantity': 100,
-        'price': 20,
-        'results': {'trades': 1000},
-    },
-    '2015-01-04': {
-        'quantity': 0,
-        'price': 0,
-        'results': {'trades': 1000},
-    },
-    '2015-01-03': {
-        'quantity': 100,
-        'price': 10,
-        'results': {},
-    },
-    '2015-01-02': {
-        'quantity': 0,
-        'price': 0,
-        'results': {},
-    },
-    '2015-01-01': {
-        'quantity': 100,
-        'price': 10,
-        'results': {},
-    }
+    '2015-01-06': EXPECTED_STATE11,
+    '2015-01-05': EXPECTED_STATE10,
+    '2015-01-04': EXPECTED_STATE7,
+    '2015-01-03': EXPECTED_STATE9,
+    '2015-01-02': EXPECTED_STATE1,
+    '2015-01-01': EXPECTED_STATE9
 }
 
 EXPECTED_LOG5 = {
-    '2015-01-06': {
-        'quantity': 50,
-        'price': 20,
-        'results': {'trades': 2000},
-    },
-    '2015-01-05': {
-        'quantity': 100,
-        'price': 20,
-        'results': {'trades': 1000},
-    },
-    '2015-01-04': {
-        'quantity': 0,
-        'price': 0,
-        'results': {'trades': 1000},
-    },
-    '2015-01-03': {
-        'quantity': 100,
-        'price': 10,
-        'results': {},
-    },
-    '2015-01-02': {
-        'quantity': 0,
-        'price': 0,
-        'results': {},
-    },
-    '2015-01-01': {
-        'quantity': 100,
-        'price': 10,
-        'results': {},
-    }
+    '2015-01-06': EXPECTED_STATE12,
+    '2015-01-05': EXPECTED_STATE10,
+    '2015-01-04': EXPECTED_STATE7,
+    '2015-01-03': EXPECTED_STATE9,
+    '2015-01-02': EXPECTED_STATE1,
+    '2015-01-01': EXPECTED_STATE9
 }
 
 EXPECTED_LOG6 = {
-    '2015-01-02': {
-        'quantity': -50,
-        'price': 20,
-        'results': {'trades': 500},
-    },
+    '2015-01-02': EXPECTED_STATE13,
     '2015-01-01': {
         'quantity': 50,
         'price': 10,
@@ -163,159 +63,51 @@ EXPECTED_LOG6 = {
 }
 
 EXPECTED_LOG7 = {
-    '2015-01-02': {
-        'quantity': 0,
-        'price': 0,
-        'results': {},
-    },
-    '2015-01-01': {
-        'quantity': -100,
-        'price': 10,
-        'results': {},
-    }
+    '2015-01-02': EXPECTED_STATE1,
+    '2015-01-01': EXPECTED_STATE2
 }
 
 EXPECTED_LOG8 = {
-    '2015-01-03': {
-        'quantity': -100,
-        'price': 10,
-        'results': {},
-    },
-    '2015-01-02': {
-        'quantity': 0,
-        'price': 0,
-        'results': {},
-    },
-    '2015-01-01': {
-        'quantity': -100,
-        'price': 10,
-        'results': {},
-    }
+    '2015-01-03': EXPECTED_STATE2,
+    '2015-01-02': EXPECTED_STATE1,
+    '2015-01-01': EXPECTED_STATE2
 }
 
 EXPECTED_LOG9 = {
-    '2015-01-05': {
-        'quantity': -100,
-        'price': 20,
-        'results': {'trades': -1000},
-    },
-    '2015-01-04': {
-        'quantity': 0,
-        'price': 0,
-        'results': {'trades': -1000},
-    },
-    '2015-01-03': {
-        'quantity': -100,
-        'price': 10,
-        'results': {},
-    },
-    '2015-01-02': {
-        'quantity': 0,
-        'price': 0,
-        'results': {},
-    },
-    '2015-01-01': {
-        'quantity': -100,
-        'price': 10,
-        'results': {},
-    }
+    '2015-01-05': EXPECTED_STATE4,
+    '2015-01-04': EXPECTED_STATE3,
+    '2015-01-03': EXPECTED_STATE2,
+    '2015-01-02': EXPECTED_STATE1,
+    '2015-01-01': EXPECTED_STATE2
 }
 
 EXPECTED_LOG10 = {
-    '2015-01-06': {
-        'quantity': 0,
-        'price': 0,
-        'results': {'trades': -3000},
-    },
-    '2015-01-05': {
-        'quantity': -100,
-        'price': 20,
-        'results': {'trades': -1000},
-    },
-    '2015-01-04': {
-        'quantity': 0,
-        'price': 0,
-        'results': {'trades': -1000},
-    },
-    '2015-01-03': {
-        'quantity': -100,
-        'price': 10,
-        'results': {},
-    },
-    '2015-01-02': {
-        'quantity': 0,
-        'price': 0,
-        'results': {},
-    },
-    '2015-01-01': {
-        'quantity': -100,
-        'price': 10,
-        'results': {},
-    }
+    '2015-01-06': EXPECTED_STATE5,
+    '2015-01-05': EXPECTED_STATE4,
+    '2015-01-04': EXPECTED_STATE3,
+    '2015-01-03': EXPECTED_STATE2,
+    '2015-01-02': EXPECTED_STATE1,
+    '2015-01-01': EXPECTED_STATE2
 }
 
 EXPECTED_LOG11 = {
-    '2015-01-04': {
-        'quantity': 0,
-        'price': 0,
-        'results': {'trades': -1000},
-    },
-    '2015-01-03': {
-        'quantity': -100,
-        'price': 10,
-        'results': {},
-    },
-    '2015-01-02': {
-        'quantity': 0,
-        'price': 0,
-        'results': {},
-    },
-    '2015-01-01': {
-        'quantity': -100,
-        'price': 10,
-        'results': {},
-    }
+    '2015-01-04': EXPECTED_STATE3,
+    '2015-01-03': EXPECTED_STATE2,
+    '2015-01-02': EXPECTED_STATE1,
+    '2015-01-01': EXPECTED_STATE2
 }
 
 EXPECTED_LOG12 = {
-    '2015-01-06': {
-        'quantity': -50,
-        'price': 20,
-        'results': {'trades': -2000},
-    },
-    '2015-01-05': {
-        'quantity': -100,
-        'price': 20,
-        'results': {'trades': -1000},
-    },
-    '2015-01-04': {
-        'quantity': 0,
-        'price': 0,
-        'results': {'trades': -1000},
-    },
-    '2015-01-03': {
-        'quantity': -100,
-        'price': 10,
-        'results': {},
-    },
-    '2015-01-02': {
-        'quantity': 0,
-        'price': 0,
-        'results': {},
-    },
-    '2015-01-01': {
-        'quantity': -100,
-        'price': 10,
-        'results': {},
-    }
+    '2015-01-06': EXPECTED_STATE6,
+    '2015-01-05': EXPECTED_STATE4,
+    '2015-01-04': EXPECTED_STATE3,
+    '2015-01-03': EXPECTED_STATE2,
+    '2015-01-02': EXPECTED_STATE1,
+    '2015-01-01': EXPECTED_STATE2
 }
 
 EXPECTED_LOG13 = {
-    '2015-01-02': {
-        'quantity': 50,
-        'price': 10,
-        'results': {'trades': 500},
-    },
+    '2015-01-02': EXPECTED_STATE8,
     '2015-01-01': {
         'quantity': -50,
         'price': 20,
@@ -324,11 +116,7 @@ EXPECTED_LOG13 = {
 }
 
 EXPECTED_LOG14 = {
-    '2015-01-02': {
-        'quantity': 0,
-        'price': 0,
-        'results': {'trades': 1000},
-    },
+    '2015-01-02': EXPECTED_STATE7,
     '2015-01-01': {
         'quantity': -100,
         'price': 20,
@@ -337,90 +125,38 @@ EXPECTED_LOG14 = {
 }
 
 EXPECTED_LOG15 = {
-    '2015-01-01': {
-        'quantity': 100,
-        'price': 10,
-        'results': {},
-    }
+    '2015-01-01': EXPECTED_STATE9
 }
 
 EXPECTED_LOG16 = {
-    '2015-01-01': {
-        'quantity': 0,
-        'price': 0,
-        'results': {'daytrades': 1000},
-    }
+    '2015-01-01': EXPECTED_STATE25
 }
 
 EXPECTED_LOG17 = {
-    '2015-09-24': {
-        'price': 5.0,
-        'quantity': 200,
-        'results': {'trades': 1200},
-    }
+    '2015-09-24': EXPECTED_STATE0
 }
 
 EXPECTED_LOG18 = {
-    '2015-09-25': {
-        'price': 5.0,
-        'quantity': 200,
-        'results': {'trades': 1200},
-    },
-    '2015-09-24': {
-        'price': 5.0,
-        'quantity': 200,
-        'results': {'trades': 1200},
-    }
+    '2015-09-25': EXPECTED_STATE0,
+    '2015-09-24': EXPECTED_STATE0
 }
 
 EXPECTED_LOG19 = {
-    '2015-01-01': {
-        'quantity': 100,
-        'price': 10,
-        'results': {'daytrades': 1000},
-    }
+    '2015-01-01': EXPECTED_STATE23
 }
 
 EXPECTED_LOG20 = {
-    '2015-01-03': {
-        'quantity': 100,
-        'price': 10,
-        'results': {'daytrades': 1000},
-    },
-    '2015-01-02': {
-        'quantity': 100,
-        'price': 10,
-        'results': {'daytrades': 1000},
-    },
-    '2015-01-01': {
-        'quantity': 0,
-        'price': 0,
-        'results': {'daytrades': 1000},
-    }
+    '2015-01-03': EXPECTED_STATE23,
+    '2015-01-02': EXPECTED_STATE23,
+    '2015-01-01': EXPECTED_STATE25
 }
 
 EXPECTED_LOG21 = {
-    '2015-01-02': {
-        'quantity': 100,
-        'price': 10,
-        'results': {'daytrades': 2000}
-    },
-    '2015-01-01': {
-        'quantity': 0,
-        'price': 0,
-        'results': {'daytrades': 1000}
-    }
+    '2015-01-02': EXPECTED_STATE24,
+    '2015-01-01': EXPECTED_STATE25
 }
 
 EXPECTED_LOG23 = {
-    '2015-01-02': {
-        'quantity': 100,
-        'price': 10,
-        'results': {'daytrades': 1000}
-    },
-    '2015-01-01': {
-        'quantity': 0,
-        'price': 0,
-        'results': {'daytrades': 1000}
-    }
+    '2015-01-02': EXPECTED_STATE23,
+    '2015-01-01': EXPECTED_STATE25
 }
